ec94fcd1c7851f14327bdd2119a13dcb745e1d26
JohnJamesPulka <yagazhigler@gmail.com>
2022-05-30 13:36:53 +0100

    nav bar menu and sub menu
    creating the sub-menu with on click function

--- home.js
+++ home.js
@@ -1,4 +1,3 @@
-
 var slideIndex =0;
 carousel();
 
@@ -18,26 +17,30 @@ function carousel(){
 
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
+
+
 function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
-  
-  
-  /*
-  // Close the dropdown if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+function showSubMenu() {
+  document.getElementById("myHtml").classList.toggle("show");
+}
+
+function Function() {
+  document.getElementById("myCss").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
       }
     }
   }
-
-  */
-
-console.log('hello world');
+}
